ccc0900505aa1ea08666bbfd39302c67a797fa28
unknown <livistteddy@gmail.com>
2021-05-20 22:47:45 +0100

    Create basic scraper function

--- scraper.rb
+++ scraper.rb
@@ -3,5 +3,10 @@ require 'nokogiri'
 require 'byebug'
 
 def my_scraper
-  
+  url = "https://www.weather-forecast.com/locations/Paris/forecasts/latest"
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  byebug
 end
+
+my_scraper

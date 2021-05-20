53f41352cf1f33a65341269d9ce50fe52267ca00
unknown <livistteddy@gmail.com>
2021-05-21 00:16:47 +0100

    Create weather array for paris

--- scraper.rb
+++ scraper.rb
@@ -6,6 +6,15 @@ def my_scraper
   url = "https://www.weather-forecast.com/locations/Paris/forecasts/latest"
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
+  weathers = Array.new
+  paris_weathers = parsed_page.css('td.b-forecast__table-description-cell--js')
+  paris_weathers.each do |paris_weather|
+    weather = {
+      weather_title: paris_weather.css('div.b-forecast__table-description-title').text,
+      weather_description: paris_weather.css('span.phrase').text
+    }
+    weathers << weather
+  end
   byebug
 end
 

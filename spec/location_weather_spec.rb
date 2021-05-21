735920c724bcc240e6609e94ea13d4264271d07a
unknown <livistteddy@gmail.com>
2021-05-21 22:35:22 +0100

    Reduce call to website in test to once

--- spec/location_weather_spec.rb
+++ spec/location_weather_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../lib/location_weather.rb'
 
 describe LocationWeather do
+  new_instance = LocationWeather.new('Lagos')
   describe "#initialize" do
     context "when instantiated" do
       it "also creates an instance of Parsers class" do
-        new_instance = LocationWeather.new('Lagos')
         expect(new_instance.requested_weather).to be_an_instance_of Parsers
       end
     end
@@ -13,8 +13,8 @@ describe LocationWeather do
   describe "#some_location" do
     context "when run" do
       it "adds items to array" do
-        new_instance = LocationWeather.new('Ikeja')
-        expect(new_instance.weathers.count).to eql(0)
+        new_instance.some_location
+        expect(new_instance.weathers.count).to eql(4)
       end
     end
   end

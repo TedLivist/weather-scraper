74ada168e7a368edb2eed5db8063f2dd79848bbf
Teddy <livistteddy@gmail.com>
2021-05-21 17:39:29 +0100

    Refactor LocationWeather class

---
+++ lib/location_weather.rb
@@ -0,0 +1,20 @@
+require_relative './parsers.rb'
+
+class LocationWeather
+  attr_reader :weathers, :requested_weather
+  def initialize(city)
+    @weathers = Array.new
+    @requested_weather = Parsers.new(city)
+  end
+
+  def some_location
+    paris_weathers = @requested_weather.parsed_page.css('td.b-forecast__table-description-cell--js')
+    paris_weathers.each do |paris_weather|
+      weather = {
+        weather_title: paris_weather.css('div.b-forecast__table-description-title').text,
+        weather_description: paris_weather.css('span.phrase').text
+      }
+      weathers << weather
+    end
+  end
+end
--- scraper.rb
+++ scraper.rb
@@ -1,38 +1,33 @@
 require_relative './lib/parsers.rb'
+require_relative './lib/location_weather.rb'
 
 def my_scraper
-  the_parse = Parsers.new('Paris')
-  weathers = Array.new
-  paris_weathers = the_parse.parsed_page.css('td.b-forecast__table-description-cell--js')
-  paris_weathers.each do |paris_weather|
-    weather = {
-      weather_title: paris_weather.css('div.b-forecast__table-description-title').text,
-      weather_description: paris_weather.css('span.phrase').text
-    }
-    weathers << weather
-  end
+  the_city = gets.chomp
+  the_parse = Parsers.new(the_city)
+  main_weather = LocationWeather.new(the_city)
+
+  # paris_weathers = the_parse.parsed_page.css('td.b-forecast__table-description-cell--js')
+  # paris_weathers.each do |paris_weather|
+  #   weather = {
+  #     weather_title: paris_weather.css('div.b-forecast__table-description-title').text,
+  #     weather_description: paris_weather.css('span.phrase').text
+  #   }
+  #   main_weather.weathers << weather
+  # end
+  main_weather.some_location
 
   puts "    " + the_parse.parsed_page.css('span.show-for-medium-up').text + "\n"
-  for i in weathers do
+  for i in main_weather.weathers do
     puts "\n"
     puts i[:weather_title] + "\n" + i[:weather_description]
   end
 
   near_paris_table_header = the_parse.parsed_page.css('table.other_places.guide th')
-  # near_paris_table_header.each do |near_paris_location_header|
-  #   puts "| #{near_paris_location_header.text} |"
-  # end
   
   near_paris_cities_data = the_parse.parsed_page.css('table.other_places.guide tr td b')
   the_cities_data = the_parse.parsed_page.css('table.other_places tr td span.phrase')
-  # print the_cities_data
-  c_n_p = 1
+  
   s = 0
-  e = 3
-  # near_paris_table_header[second..last].each do |i|
-  #   puts i.text
-  # end
-
 
   # Weather forecast for places near #{Paris}
   puts the_parse.parsed_page.css('h3.nearest-other-header').text << "\n"
@@ -42,23 +37,8 @@ def my_scraper
        puts "#{x.text}: #{the_cities_data[s].text}"
        s += 1
     end
-    # puts "#{near_paris_table_header[1].text}: #{the_cities_data[s].text}"
-    # puts "#{near_paris_table_header[2].text}: #{the_cities_data[s + 1].text}"
-    # puts "#{near_paris_table_header[3].text}: #{the_cities_data[s + 2].text}"
-    # puts "#{near_paris_table_header[4].text}: #{the_cities_data[s + 3].text}"
     puts "\n"
   end
-
-
-
-  # puts near_paris_cities_data[c_n_p].text
-  # the_cities_data[s..e].each do |x|
-  #   puts x.text
-  # end
-  # byebug
-
-
-
 
 end
 

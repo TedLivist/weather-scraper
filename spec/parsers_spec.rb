2d97292ecff3eed5e20f387edeb8fad462ed960e
unknown <livistteddy@gmail.com>
2021-05-21 19:35:41 +0100

    Complete tests for Parsers class

--- spec/parsers_spec.rb
+++ spec/parsers_spec.rb
@@ -3,9 +3,13 @@ require_relative '../lib/parsers.rb'
 describe Parsers do
   describe "#initialize" do
     context "when city is passed" do
+      new_inst = Parsers.new('Paris')
       it "formats the correct url with the city" do
-        new_inst = Parsers.new('Paris')
         expect(new_inst.url).to eql("https://www.weather-forecast.com/locations/Paris/forecasts/latest")
+      end
+
+      it "HTTParty body is a String" do
+        expect(new_inst.unparsed_page.body).to be_an_instance_of String
       end
     end
   end

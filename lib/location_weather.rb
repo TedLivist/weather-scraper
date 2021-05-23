4f348f0f6b5d8c65d8ccd798fd73219aa85d41c1
unknown <livistteddy@gmail.com>
2021-05-23 07:48:22 +0100

    Create private method in LocationWeather class

--- lib/location_weather.rb
+++ lib/location_weather.rb
@@ -1,7 +1,7 @@
 require_relative './parsers'
 
 class LocationWeather < Parsers
-  attr_reader :weathers, :requested_weather
+  attr_reader :weathers
 
   def initialize(city)
     @weathers = []
@@ -9,6 +9,11 @@ class LocationWeather < Parsers
   end
 
   def add_city_weather
+    create_city_weather
+  end
+
+  private
+  def create_city_weather
     the_city_weathers = parsed_page.css('td.b-forecast__table-description-cell--js')
     the_city_weathers.each do |the_city_weather|
       weather = {

59a3e3482484804bb213d0820fe76f7dac771a43
unknown <livistteddy@gmail.com>
2021-05-23 08:26:15 +0100

    Add negative test for when city returns nothing

--- spec/location_weather_spec.rb
+++ spec/location_weather_spec.rb
@@ -2,14 +2,7 @@ require_relative '../lib/location_weather'
 
 describe LocationWeather do
   new_instance = LocationWeather.new('Lagos')
-  describe '#initialize' do
-    context 'when instantiated' do
-      it 'instance class inherits from Parsers class' do
-        expect(new_instance.class).to be < Parsers
-      end
-    end
-  end
-
+  some_instance = LocationWeather.new('Santorini')
   describe '#add_city_weather' do
     context 'when not run' do
       it 'weathers array is empty' do
@@ -21,6 +14,20 @@ describe LocationWeather do
       it 'adds items to array' do
         new_instance.add_city_weather
         expect(new_instance.weathers.count).to eql(4)
+      end
+    end
+  end
+
+  describe '#initialize' do
+    context 'when instantiated' do
+      it 'instance class inherits from Parsers class' do
+        expect(new_instance.class).to be < Parsers
+      end
+    end
+
+    context 'when information about city cannot be retrieved' do
+      it 'weathers array is empty' do
+        expect(some_instance.weathers.count).to eql(4)
       end
     end
   end
